7cd3aaadad7355e484b3debd28bd3e83b48eeb3a
Alejandro <alejandro.ortiz.pichardo@gmail.com>
2023-08-24 07:01:13 -0600

    border radius and size done

---
+++ src/Components/Card.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import QR from '../images/image-qr-code.png';
+import './Card.css';
+
+function Card({title, body}) {
+  return (
+    <div className='container'>
+    <div className= 'card-container'>
+      <div className='image-container'>
+        <img src={QR} alt="My QR" />
+      </div>
+      <div className='card-content'>
+      <div className='card-title'>
+        <h3>{title}</h3>
+      </div>
+      <div className='card-body'>
+        <p>{body}</p>
+      </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Card
