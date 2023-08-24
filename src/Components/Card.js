15725ddf3eae2e9548ce2ea67ce53d015928a0da
Alejandro <alejandro.ortiz.pichardo@gmail.com>
2023-08-24 10:54:06 -0600

    screen size

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,6 +6,7 @@ function Card({title, body}) {
   return (
     <div className='container col-xxl-12'>
     <div className= 'card-container'>
+        <a href='https://open.spotify.com/intl-es/track/4cOdK2wGLETKBW3PvgPWqT?si=17d5b7b591ab4b84' target="_blank" >
       <div className='image-container'>
         <img src={QR} alt="My QR" />
       </div>
@@ -17,6 +18,7 @@ function Card({title, body}) {
         <p>{body}</p>
       </div>
       </div>
+      </a>
     </div>
     </div>
   )
